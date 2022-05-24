d0c098cb2426e02dde955664cccb538924c13545
Julio Costa <julio.costa@bitcapital.com.br>
2022-05-24 15:19:04 -0300

    feat: http server with createReadStream

--- example01-common-scenarios/index.mjs
+++ example01-common-scenarios/index.mjs
@@ -15,3 +15,19 @@ function neoStdin() {
   2. GERANDO UM ARQUIVO GRANDE
   node -e "process.stdout.write(crypto.randomBytes(1e9))" > big.file
 */
+
+import http from 'http';
+import { readFileSync, createReadStream } from 'fs';
+
+http.createServer((req, res) => {
+  // const file = readFileSync('big.file');
+  // res.write(file);
+  // res.end()
+
+  createReadStream('big.file')
+  .pipe(res)
+})
+.listen(3007)
+.on('listening', () => console.log('server is listening at 3007'));
+
+// curl localhost:3007 --output output.big
